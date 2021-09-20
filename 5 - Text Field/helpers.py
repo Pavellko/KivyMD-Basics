339869145c56ee98eaba637412d521ed76b5c55c
Pavellko <pavelkollegov@gmail.com>
2021-09-20 10:18:11 +0300

    v1

--- 5 - Text Field/helpers.py
+++ 5 - Text Field/helpers.py
@@ -5,7 +5,7 @@ MDTextField:
     helper_text_mode: "on_focus"
     icon_right: "android"
     icon_right_color: app.theme_cls.primary_color
-    pos_hint:{'center_x': 0.5, 'center_y': 0.5}
+    pos_hint:{'center_x': 0.5, 'center_y': 0.6}
     size_hint_x:None
     width:300
 """

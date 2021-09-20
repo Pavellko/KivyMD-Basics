9c0b206f242a8077439047d63448d1408a11971b
Pavellko <pavelkollegov@gmail.com>
2021-09-20 10:41:03 +0300

    v3

--- 6 - Binding Input and button/inputwithbutton.py
+++ 6 - Binding Input and button/inputwithbutton.py
@@ -3,6 +3,8 @@ from kivymd.uix.screen import Screen
 from kivymd.uix.button import MDRectangleFlatButton
 from kivy.lang import Builder
 import helpers
+from kivymd.uix.label import MDLabel, MDIcon
+from kivymd.font_definitions import theme_font_styles
 
 
 class DemoApp(MDApp):
@@ -10,16 +12,21 @@ class DemoApp(MDApp):
         self.theme_cls.primary_palette = "Green"
         screen = Screen()
 
-        username = Builder.load_string(helpers.username_input)
+        self.username = Builder.load_string(helpers.username_input)
+
+        self.label = MDLabel(text="Hello world", halign="center", theme_text_color="Custom",
+                text_color=(0,0,1,1), font_style="Subtitle2", pos_hint={'center_x': 0.5, 'center_y': 0.3})
         button = MDRectangleFlatButton(text='Show',
                                        pos_hint={'center_x': 0.5, 'center_y': 0.4},
-                                       on_release=self.show_data)
+                                       on_release=self.show)
+        screen.add_widget(self.label)
         screen.add_widget(self.username)
         screen.add_widget(button)
         return screen
 
-    def show_data(self,obj):
-        print(self.username.text)
+    def show(self, args):
+
+        self.label.text = self.username.text
 
 
 DemoApp().run()
